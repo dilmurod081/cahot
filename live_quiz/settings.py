3e86e2af9e5b3bc1f1e82ed4de295ece79126697
dilmurod081 <141741043+dilmurod081@users.noreply.github.com>
2025-09-21 08:57:21 +0500

    Update settings.py

--- live_quiz/settings.py
+++ live_quiz/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-js&sn+$g)9%7=gnsze)d8cxt@rh8r-(=*#w^)x18$uj9-+#@)8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
